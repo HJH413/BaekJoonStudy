ad5cd94de51c1eae25196963eeef042dc259115c
HJH413 <413hjh413@gmail.com>
2022-07-17 22:46:55 +0900

    https://www.acmicpc.net/step/1 입출력과 사칙연산

---
+++ src/java220717/Main.java
@@ -0,0 +1,98 @@
+package java220717;
+import java.util.Scanner;
+//2022 07 17
+public class Main {
+    //1번 문제
+//    public static void main(String[] args) {
+//        System.out.print("Hello World!");
+//    }
+
+    //2번 문제
+//    public static void main(String[] args) {
+//        System.out.println("강한친구 대한육군");
+//        System.out.println("강한친구 대한육군");
+//    }
+
+    // 3번문제
+//    public static void main(String[] args) {
+//        System.out.print("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+//    }
+    //4번문제
+//    public static void main(String[] args) {
+//        System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+//    }
+    //5번문제
+//    public static void main(String[] args) {
+//        //사용자 입력값 받아오기
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        System.out.print(A+B);
+//    }
+    //6번문제
+//    public static void main(String[] args) {
+//        //사용자 입력값 받아오기
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        System.out.print(A-B);
+//    }
+    //7번문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        System.out.print(A*B);
+//    }
+    //8번문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        Double A = sc.nextDouble();
+//        Double B = sc.nextDouble();
+//        System.out.print(A/B);
+//    }
+    //9번문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        System.out.println(A+B);
+//        System.out.println(A-B);
+//        System.out.println(A*B);
+//        System.out.println(A/B);
+//        System.out.println(A%B);
+//    }
+    //10번 문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String a = sc.next();
+//        System.out.print(a+"??!");
+//    }
+    //11번 문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        int C = sc.nextInt();
+//        System.out.println((A + B) % C);
+//        System.out.println(((A % C) + (B % C)) % C);
+//        System.out.println((A * B) % C);
+//        System.out.println(((A % C) * (B % C)) % C);
+//    }
+    //12번 문제
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        String C = String.valueOf(B);
+//        char[] D = C.toCharArray();
+//        for (int i = D.length-1; i >= 0; i--) {
+//            System.out.println(A*Integer.parseInt(String.valueOf(D[i])));
+//        }
+//        System.out.println(A*B);
+//    }
+    //13번 문제
+    public static void main(String[] args) {
+        System.out.printf("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+    }
+}
